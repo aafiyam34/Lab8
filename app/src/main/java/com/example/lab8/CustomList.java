a49460ca4858fe9bded7adc9d84f495215b73d42
Aafiya Muneer <aafiyam34@gmail.com>
2024-10-25 12:32:23 -0600

    Added failing test for hasCity.

--- app/src/main/java/com/example/lab8/CustomList.java
+++ app/src/main/java/com/example/lab8/CustomList.java
@@ -29,8 +29,8 @@ public class CustomList extends ArrayAdapter<City> {
 
         View view = convertView;
 
-        if(view == null){
-            view = LayoutInflater.from(context).inflate(R.layout.content, parent,false);
+        if (view == null) {
+            view = LayoutInflater.from(context).inflate(R.layout.content, parent, false);
         }
 
         City city = cities.get(position);
@@ -45,11 +45,11 @@ public class CustomList extends ArrayAdapter<City> {
 
     }
 
-    public int getCount(){
+    public int getCount() {
         return cities.size();
     }
 
-    public void addCity(City city){
+    public void addCity(City city) {
         cities.add(city);
 
     }
